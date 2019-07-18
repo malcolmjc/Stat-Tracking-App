9808043b0420d6d3b6858922706593d1ee16f3b7
Malcolm Craney <mjcraney@calpoly.edu>
2019-07-17 20:58:40 -0700

    Update App Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,42 +1,60 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from "./auth/auth.guard";
 import { GameCreateListComponent } from './game/game-create-list/game-create-list.component';
 import { GameListComponent } from './game/game-list/game-list.component';
 import { GamePlayerSelectComponent } from './game/game-player-select/game-player-select.component';
+import { LoginComponent } from "./auth/login/login.component";
 import { MatchupPredictorComponent } from './user/matchup-predictor/matchup-predictor.component';
 import { PredictorStatsComponent } from './user/predictor-stats/predictor-stats.component';
+import { SignupComponent } from "./auth/signup/signup.component";
 import { UserStatsComponent } from './user/user-stats/user-stats.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: GameListComponent
+    component: GameListComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'create',
-    component: GameCreateListComponent
+    component: GameCreateListComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'leaderboard',
-    component: UserStatsComponent
+    component: UserStatsComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'select-players',
-    component: GamePlayerSelectComponent
+    component: GamePlayerSelectComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'predict-matchup',
-    component: MatchupPredictorComponent
+    component: MatchupPredictorComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'predict-matchup/stats',
-    component: PredictorStatsComponent
-  }
+    component: PredictorStatsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthInterceptor } from './auth/auth-interceptor';
+import { AuthModule } from './auth/auth.module';
 import { GameModule } from './game/game.module';
 import { HeaderModule } from './header/header.module';
 import { UserModule } from './user/user.module';
@@ -19,9 +21,13 @@ import { UserModule } from './user/user.module';
     HttpClientModule,
 
     AppRoutingModule,
+    AuthModule,
     GameModule,
     HeaderModule,
     UserModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
