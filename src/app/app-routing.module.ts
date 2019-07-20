a63624b2650546844330e0f177522cbd3c42395a
Malcolm Craney <mjcraney@calpoly.edu>
2019-07-20 11:51:59 -0700

    Nested Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,46 +1,48 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthGuard } from "./auth/auth.guard";
+import { AppRootComponent } from './app-root.component';
+import { AuthGuard } from './auth/auth.guard';
 import { GameCreateListComponent } from './game/game-create-list/game-create-list.component';
 import { GameListComponent } from './game/game-list/game-list.component';
 import { GamePlayerSelectComponent } from './game/game-player-select/game-player-select.component';
-import { LoginComponent } from "./auth/login/login.component";
+import { LoginComponent } from './auth/login/login.component';
 import { MatchupPredictorComponent } from './user/matchup-predictor/matchup-predictor.component';
 import { PredictorStatsComponent } from './user/predictor-stats/predictor-stats.component';
-import { SignupComponent } from "./auth/signup/signup.component";
+import { SignupComponent } from './auth/signup/signup.component';
 import { UserStatsComponent } from './user/user-stats/user-stats.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: GameListComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'create',
-    component: GameCreateListComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'leaderboard',
-    component: UserStatsComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'select-players',
-    component: GamePlayerSelectComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'predict-matchup',
-    component: MatchupPredictorComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'predict-matchup/stats',
-    component: PredictorStatsComponent,
-    canActivate: [AuthGuard]
+    component: AppRootComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        component: GameListComponent
+      },
+      {
+        path: 'create',
+        component: GameCreateListComponent
+      },
+      {
+        path: 'leaderboard',
+        component: UserStatsComponent,
+      },
+      {
+        path: 'select-players',
+        component: GamePlayerSelectComponent,
+      },
+      {
+        path: 'predict-matchup',
+        component: MatchupPredictorComponent,
+      },
+      {
+        path: 'predict-matchup/stats',
+        component: PredictorStatsComponent,
+      }
+    ]
   },
   {
     path: 'login',
@@ -50,6 +52,10 @@ const routes: Routes = [
     path: 'signup',
     component: SignupComponent
   },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
 
 @NgModule({
