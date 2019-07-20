012bce3629a1df226e0dcc0917183e864a63d735
Malcolm Craney <mjcraney@calpoly.edu>
2019-07-20 13:52:06 -0700

    Move Game Routes to Separate File

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const Game = require('./model/game').model;
 
 const userRoutes = require("./routes/user");
+const gameRoutes = require("./routes/game");
 
 mongoose.connect(
   'mongodb+srv://malcolmjc:uR2P7vVIrtlVhwVr@cluster0-85oau.mongodb.net/dye-stats',
@@ -31,60 +32,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const checkAuth = require("./middleware/check-auth");
-const User = require("./model/user");
-app.post(
-  '/api/games/add',
-  checkAuth,
-  (req, res, next) => {
-    console.log('adding game');
-    User.findById(req.body.userId, 'stats games').then((user) => {
-      const game = new Game({
-        date: req.body.date,
-        playerGames: req.body.playerGames,
-        winners: req.body.winners,
-        losers: req.body.losers,
-        score: req.body.score
-      });
-      user.games.push(game);
-      // TODO: update stats
-      user.save().then((data) => {
-        res.status(201).json({
-           message: 'game added',
-           id: game._id
-        });
-      });
-    });
-});
-
-app.post(
-  '/api/games/get',
-  checkAuth,
-  (req, res, next) => {
-    console.log('getting games');
-    User.findById(req.body.userId, 'games')
-      .then(user => {
-        res.status(200).json({
-          message: 'games fetched',
-          games: user.games
-        });
-      });
-  });
-
-app.delete(
-  '/api/games/:userId/:gameId',
-  checkAuth,
-  (req, res, next) => {
-    User.findById(req.params.userId, 'games').then((user) => {
-      user.games.id(req.params.gameId).remove();
-      user.save().then((user) => {
-        res.status(200).json({
-          message: 'game deleted'
-        });
-      });
-    });
-  });
-
 app.use('/api/user', userRoutes);
+app.use('/api/games', gameRoutes);
 
 module.exports = app;
