341d3f257dc25851b996f9bd7a6a61ae8f307417
Malcolm Craney <mjcraney@calpoly.edu>
2019-07-28 19:13:48 -0700

    Module updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
+import { ToastrModule } from 'ngx-toastr';
+
+import { AppRootComponent } from './app-root.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { AuthModule } from './auth/auth.module';
 import { GameModule } from './game/game.module';
+import { GroupsModule } from './groups/groups.module';
 import { HeaderModule } from './header/header.module';
 import { UserModule } from './user/user.module';
-import { AppRootComponent } from './app-root.component';
 
 @NgModule({
   declarations: [
@@ -25,8 +28,12 @@ import { AppRootComponent } from './app-root.component';
     AppRoutingModule,
     AuthModule,
     GameModule,
+    GroupsModule,
     HeaderModule,
-    UserModule
+    UserModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-top-center',
+    })
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
