550f63d4f0b835a12b0cd8440db17b1fa57c54f0
Malcolm Craney <mjcraney@calpoly.edu>
2019-07-28 19:17:53 -0700

    Create group component

---
+++ src/app/groups/create-group/create-group.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { GroupService } from '../group.service';
+
+@Component({
+  selector: 'app-create-group',
+  templateUrl: './create-group.component.html',
+  styleUrls: ['./create-group.component.css']
+})
+export class CreateGroupComponent {
+  constructor(private groupService: GroupService, private router: Router) {}
+
+  onCreateGroup(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.groupService.createGroup(form.value.groupName, form.value.password, form.value.slogan, form.value.description);
+    this.router.navigate(['/my-groups']);
+  }
+}
