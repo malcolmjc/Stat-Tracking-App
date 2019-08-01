346108ce96aeebe3d94064785fa052e3b797cae4
Malcolm Craney <mjcraney@calpoly.edu>
2019-07-31 22:17:31 -0700

    Game List Updates

--- src/app/game/game-list/game-list.component.ts
+++ src/app/game/game-list/game-list.component.ts
@@ -4,6 +4,8 @@ import { Subscription } from 'rxjs';
 
 import { Game } from '../game.model';
 import { GameService } from '../game.service';
+import { GameDisplay } from '../game-display.model';
+import { PlayerGame } from '../player-game.model';
 
 @Component({
   selector: 'app-game-list',
@@ -13,7 +15,9 @@ import { GameService } from '../game.service';
 export class GameListComponent implements OnInit, OnDestroy {
   public gameListener: Subscription;
   public isLoading = false;
-  public games: Game[] = [];
+  public games: GameDisplay[] = [];
+  public winningScore = 0;
+  public losingScore = 0;
 
   constructor(public gameService: GameService) { }
 
@@ -22,9 +26,36 @@ export class GameListComponent implements OnInit, OnDestroy {
     this.gameService.getGames();
     this.gameListener = this.gameService.getGameUpdateListener()
       .subscribe((games: Game[]) => {
-        console.log('getting a new game');
+        const displayGames: GameDisplay[] = [];
+        games.forEach((game) => {
+          let winningScore = 0;
+          let losingScore = 0;
+          const winners: string[] = [];
+          const losers: string[] = [];
+          const winnerGames: PlayerGame[] = [];
+          const loserGames: PlayerGame[] = [];
+          game.playerGames.forEach((playerGame) => {
+            playerGame.won ? winningScore += playerGame.points : losingScore += playerGame.points;
+            if (playerGame.won) {
+              winnerGames.push(playerGame);
+              winners.push(playerGame.playerName);
+            } else {
+              loserGames.push(playerGame);
+              losers.push(playerGame.playerName);
+            }
+          });
+          displayGames.push({
+            ...game,
+            winningScore: winningScore,
+            losingScore: losingScore,
+            winners: winners,
+            losers: losers,
+            winnerGames: winnerGames,
+            loserGames: loserGames
+          });
+        });
         this.isLoading = false;
-        this.games = games.reverse();
+        this.games = displayGames.reverse();
     });
   }
 
