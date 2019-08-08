a29698db6c7f14f272dd6fa60db0f7560fde657b
Malcolm Craney <mjcraney@calpoly.edu>
2019-08-07 22:23:11 -0700

    #17 - Backend Environment Variables

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, "this-is-a-super-secret-message-should-be-hidden-TODO-make-hidden");
+    jwt.verify(token, process.env.JWT_SECRET);
     next();
   } catch (error) {
     res.status(401).json({
