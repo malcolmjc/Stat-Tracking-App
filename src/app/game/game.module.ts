12f81868c8933d8f0bb4d45035ed1903e8f2435a
Malcolm Craney <mjcraney@calpoly.edu>
2019-08-11 17:03:15 -0700

    #62 - Use No Games Component

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -4,13 +4,15 @@ import { GameCreateModule } from './game-create/game-create-module';
 import { GameCreateListModule } from './game-create-list/game-create-list.module';
 import { GameListModule } from './game-list/game-list.module';
 import { GamePlayerSelectModule } from './game-player-select/game-player-select.module';
+import { NoGamesModule } from './no-games/no-games.module';
 
 @NgModule({
   exports: [
     GameCreateModule,
     GameCreateListModule,
     GameListModule,
-    GamePlayerSelectModule
+    GamePlayerSelectModule,
+    NoGamesModule
   ]
 })
 export class GameModule { }
