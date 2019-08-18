71f6f16a0acb7e644d45e2f9871b8535741f8909
Malcolm Craney <mjcraney@calpoly.edu>
2019-08-17 21:13:35 -0700

    #14 - User Component's Mocks

---
+++ src/app/user/player-selection-form-field/player-selection-form-field.component.mock.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-player-selection-form-field',
+  template: 'MOCK player-selection-form-field',
+})
+export class MockPlayerSelectionFormFieldComponent {
+  @Input() public playerNames: string[] = [];
+}
