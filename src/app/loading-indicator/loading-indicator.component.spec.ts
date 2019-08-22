51e0fac3d1686f60c7cd5b1688941db5826c88c5
Malcolm Craney <mjcraney@calpoly.edu>
2019-08-22 16:41:40 -0700

    #14 - LoadingIndicator Tests

--- src/app/loading-indicator/loading-indicator.component.spec.ts
+++ src/app/loading-indicator/loading-indicator.component.spec.ts
@@ -1,13 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Component, ViewChild } from '@angular/core';
 
 import { MatProgressSpinnerModule } from '@angular/material';
 
 import { LoadingIndicatorComponent } from './loading-indicator.component';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: '<app-loading-indicator [fullPage]="fullPage"></app-loading-indicator>'
+})
+class TestComponent {
+  @ViewChild(LoadingIndicatorComponent) public component: LoadingIndicatorComponent;
+  public fullPage = true;
+}
 
 describe('LoadingIndicatorComponent', () => {
   let component: LoadingIndicatorComponent;
-  let fixture: ComponentFixture<LoadingIndicatorComponent>;
+  let testComponent: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,18 +26,33 @@ describe('LoadingIndicatorComponent', () => {
         BrowserAnimationsModule,
         MatProgressSpinnerModule
       ],
-      declarations: [ LoadingIndicatorComponent ]
+      declarations: [
+        LoadingIndicatorComponent,
+        TestComponent
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoadingIndicatorComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestComponent);
+    testComponent = fixture.componentInstance;
+    component = testComponent.component;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  test('should get correct classname if fullPage', () => {
+    expect(testComponent.fullPage).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('.loading-indicator-container-full'))).toBeTruthy();
+  });
+
+  test('should get correct classname if not fullPage', () => {
+    testComponent.fullPage = false;
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('.loading-indicator-container'))).toBeTruthy();
   });
 });
