42a7557fad2efe9da61327f15e7453d39f0866ff
Malcolm Craney <mjcraney@calpoly.edu>
2019-08-22 16:06:28 -0700

    #14 - Header Tests

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,10 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatToolbarModule, MatButtonModule } from '@angular/material';
-import { of } from 'rxjs';
+import { Subject } from 'rxjs';
 
 import { AuthService } from '../auth/auth.service';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { GroupService } from '../groups/group.service';
 import { HeaderComponent } from './header.component';
 
@@ -13,6 +13,10 @@ describe('HeaderComponent', () => {
   let fixture: ComponentFixture<HeaderComponent>;
   let authService: SpyObject<AuthService>;
   let groupService: SpyObject<GroupService>;
+  const groupName$ = new Subject<string>();
+  const authStatus$ = new Subject<boolean>();
+  const username = 'username';
+  const isAuthenticated = true;
 
   beforeEach(async(() => {
     authService = createSpyObject([
@@ -21,13 +25,15 @@ describe('HeaderComponent', () => {
       'getAuthStatusListener',
       'logout'
     ]);
-    authService.getAuthStatusListener.mockReturnValue(of(true));
+    authService.getUserName.mockReturnValue(username);
+    authService.getIsAuthenticated.mockReturnValue(isAuthenticated);
+    authService.getAuthStatusListener.mockReturnValue(authStatus$.asObservable());
 
     groupService = createSpyObject([
       'getCurrentGroupName',
       'getCurrentGroupListener'
     ]);
-    groupService.getCurrentGroupListener.mockReturnValue(of('fake-group-name'));
+    groupService.getCurrentGroupListener.mockReturnValue(groupName$.asObservable());
 
     TestBed.configureTestingModule({
       imports: [
@@ -52,5 +58,37 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  test('logging out actually logs you out', () => {
+    jest.spyOn(authService, 'logout');
+    component.onLogout();
+    expect(authService.logout).toHaveBeenCalled();
+  });
+
+  describe('ngOnInit behavior', () => {
+    test('sets authStatus correctly', () => {
+      expect(component.userIsAuthenticated).toEqual(isAuthenticated);
+    });
+
+    test('sets username correctly', () => {
+      expect(component.username).toEqual(username);
+    });
+
+    test('updates auth status based on auth status listener', () => {
+      expect(component.userIsAuthenticated).toBeTruthy();
+      authStatus$.next(false);
+      expect(component.userIsAuthenticated).toBeFalsy();
+    });
+
+    test('updates group name based on groupName listener', () => {
+      let groupName = 'groupName';
+      groupName$.next(groupName);
+      expect(component.groupName).toEqual(groupName);
+
+      groupName = 'another';
+      groupName$.next(groupName);
+      expect(component.groupName).toEqual(groupName);
+    });
   });
 });
