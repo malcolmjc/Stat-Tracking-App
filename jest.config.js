7f27edf78191d1454c36a4ff93f66bed8253d980
Malcolm Craney <mjcraney@calpoly.edu>
2019-08-23 15:52:43 -0700

    #14 - Update Jest Config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   setupFilesAfterEnv: [
     '<rootDir>/src/test/setup-jest.ts'
   ],
-  moduleFileExtensions: ["js", "ts", "json"]
+  moduleFileExtensions: ["js", "ts", "json"],
+  testMatch: [ "**/__tests__/**/*.[jt]s?(x)", "**/?(*.spec).[jt]s?(x)" ]
 };
