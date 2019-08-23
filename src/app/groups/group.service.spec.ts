81ff871ff7f5b2b2cd93a98b44f5a041c9a65da5
Malcolm Craney <mjcraney@calpoly.edu>
2019-08-23 14:50:22 -0700

    #14 - Passing GroupService Test

--- src/app/groups/group.service.spec.ts
+++ src/app/groups/group.service.spec.ts
@@ -1,9 +1,30 @@
-import { TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
+import { AuthService } from '../auth/auth.service';
 import { GroupService } from './group.service';
+import { ToastrService } from 'ngx-toastr';
 
 describe('GroupService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let toastr: SpyObject<ToastrService>;
+  let authService: SpyObject<AuthService>;
+
+  beforeEach(async(() => {
+    toastr = createSpyObject(['error', 'success']);
+    authService = createSpyObject(['getUserId', 'getUserName']);
+
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        { provide: AuthService, useValue: authService },
+        { provide: ToastrService, useValue: toastr },
+        GroupService
+      ]
+    })
+    .compileComponents();
+  }));
 
   it('should be created', () => {
     const service: GroupService = TestBed.get(GroupService);
