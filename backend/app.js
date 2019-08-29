8ef2b6e570e8c17ee812afcbf862820adc457fd5
Malcolm Craney <mjcraney@calpoly.edu>
2019-08-28 20:04:39 -0700

    #24 - Use /images

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -21,6 +22,7 @@ mongoose.connect(
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join('./images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
