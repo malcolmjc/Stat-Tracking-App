00d1591851c793fe398e0d6483271a1202cfb92a
Malcolm Craney <mjcraney@calpoly.edu>
2019-09-01 12:11:41 -0700

    #24 - UserProfile UI Improvements

--- src/app/user/user-profile/user-profile.component.ts
+++ src/app/user/user-profile/user-profile.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from 'src/app/auth/auth.service';
+
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  public username = '';
+
+  constructor(private authService: AuthService) { }
+
   public ngOnInit() {
-    // TODO - query param for username and get user details from api
+    this.username = this.authService.getUserName();
   }
 
   public imageSelected(image: File) {
