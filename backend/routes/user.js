824b3d2f67f78c8424a2f35addeffd44ab48ffb6
Malcolm Craney <mjcraney@calpoly.edu>
2019-09-04 22:45:12 -0700

    #89 - Backend /user/find Updates

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -169,7 +169,7 @@ router.get(
       });
     });
   });
-  
+
 router.get('/usernames/:userId/:groupId',
   checkAuth,
   checkGroup,
@@ -261,27 +261,44 @@ router.get('/find/:search',
     if (search.includes('@')) {
       // searching by email, must be exact
       User.findOne({ email: search }, 'username').then((user) => {
-        res.status(200).json({
-          message: 'user found by email',
-          users: [user.username]
+        if (!users || users.length === 0) {
+          res.status(404).json({
+            message: 'no users found by email',
+            users: null
+          });
+        } else {
+          res.status(200).json({
+            message: 'user found by email',
+            users: [user.username]
+          });
+        }
+      }).catch((error) => {
+        res.status(500).json({
+          message: 'something went wrong finding user by email',
+          error: error
+        });
+      });
+    } else {
+      // searching by username
+      User.find({ username: new RegExp(search) }, 'username').then((users) => {
+        if (!users || users.length === 0) {
+          res.status(404).json({
+            message: 'no users found by username',
+            users: null
+          });
+        } else {
+          res.status(200).json({
+            message: 'users found by username',
+            users: users.map((user) => user.username)
+          });
+        }
+      }).catch((error) => {
+        res.status(500).json({
+          message: 'something went wrong finding user by username',
+          error: error
         });
       });
     }
-
-    // searching by username
-    User.find({ username: new RegExp(search) }, 'username').then((users) => {
-      if (!users || users.length === 0) {
-        res.status(404).json({
-          message: 'no users found by username',
-          users: null
-        });
-      } else {
-        res.status(200).json({
-          message: 'users found by username',
-          users: users.map((user) => user.username)
-        });
-      }
-    });
   });
 
 router.get('/notifications/:userId',
