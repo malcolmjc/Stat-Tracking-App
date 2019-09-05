fa18b68bc51649ed3037c1decbb16da212e89fa4
Malcolm Craney <mjcraney@calpoly.edu>
2019-09-04 22:53:11 -0700

    #89 - Backend Fixup

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -261,7 +261,7 @@ router.get('/find/:search',
     if (search.includes('@')) {
       // searching by email, must be exact
       User.findOne({ email: search }, 'username').then((user) => {
-        if (!users || users.length === 0) {
+        if (!user || user.length === 0) {
           res.status(404).json({
             message: 'no users found by email',
             users: null
@@ -273,6 +273,7 @@ router.get('/find/:search',
           });
         }
       }).catch((error) => {
+        console.error(error);
         res.status(500).json({
           message: 'something went wrong finding user by email',
           error: error
